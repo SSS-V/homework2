d509d8a5e74d0faa5a9c811f91968562f2a9032d
SSS-V <167866942+SSS-V@users.noreply.github.com>
2024-04-27 02:34:37 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,8 @@
+name = 'Vladimir'
+print("Name:" + name , type(name))
+age = 23
+print("Age:", age, type(age))
+age = age + 1
+print("New Age:", age)
+is_student = True
+print("Is_student:", is_student, type(is_student))
